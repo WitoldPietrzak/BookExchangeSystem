6b2dc28d2bc32cd904a96f3a9521c41b6aca564e
WitoldPietrzak <witekpietrzak35@gmail.com>
2021-12-02 02:15:01 +0100

    Roles are now class with static fields instead of enum

--- Bookshare/src/main/java/org/bs/bookshare/model/Roles.java
+++ Bookshare/src/main/java/org/bs/bookshare/model/Roles.java
@@ -1,5 +1,7 @@
 package org.bs.bookshare.model;
 
-public enum Roles {
-    ROLE_USER, ROLE_ANMIN, ROLE_MODERATOR
+public class Roles {
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_MODERATOR = "ROLE_MODERATOR";
 }
