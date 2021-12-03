141dca97cc078f896fd102ce5821a2e08ee4918a
WitoldPietrzak <witekpietrzak35@gmail.com>
2021-12-04 00:52:46 +0100

    Updated userConverter

--- Bookshare/src/main/java/org/bs/bookshare/utils/converter/UserConverter.java
+++ Bookshare/src/main/java/org/bs/bookshare/utils/converter/UserConverter.java
@@ -1,15 +1,25 @@
 package org.bs.bookshare.utils.converter;
 
 import org.bs.bookshare.model.AppUser;
-import org.bs.bookshare.mok.dto.CreateUserRequestDTO;
-import org.bs.bookshare.mok.dto.UserListResponseDTO;
+import org.bs.bookshare.mok.dto.request.CreateUserRequestDTO;
+import org.bs.bookshare.mok.dto.response.UserAdminResponseDTO;
+import org.bs.bookshare.mok.dto.response.UserListResponseDTO;
+import org.bs.bookshare.mok.dto.response.UserResponseDTO;
 
 public class UserConverter {
-    public static AppUser userFromCreateUserRequestDTO(CreateUserRequestDTO dto){
+    public static AppUser userFromCreateUserRequestDTO(CreateUserRequestDTO dto) {
         return new AppUser(dto.getLogin(), dto.getEmail(), dto.getPassword());
     }
 
-    public static UserListResponseDTO UserListResponseDTOFromUser(AppUser user){
-        return new UserListResponseDTO(user.getId(),user.getLogin(),user.getEmail());
+    public static UserListResponseDTO userListResponseDTOFromUser(AppUser user) {
+        return new UserListResponseDTO(user.getId(), user.getLogin(), user.getEmail());
+    }
+
+    public static UserResponseDTO userResponseDTOFromUser(AppUser user) {
+        return new UserResponseDTO(user.getId(), user.getLogin(), user.getEmail(), user.getVersion());
+    }
+
+    public static UserAdminResponseDTO userAdminResponseDTOFromUser(AppUser user) {
+        return new UserAdminResponseDTO(user.getId(),user.getLogin(),user.getEmail(),user.getActivated(),user.getDisabled(), user.getVersion(), user.getAppRoles());
     }
 }
