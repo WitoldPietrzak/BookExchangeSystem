346a985f3e128dba55cb9893147213fba2e7da52
WitoldPietrzak <witekpietrzak35@gmail.com>
2021-12-05 06:30:48 +0100

    Moved token expire time to properties

--- Bookshare/src/main/java/org/bs/bookshare/security/SecurityConfiguration.java
+++ Bookshare/src/main/java/org/bs/bookshare/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.bs.bookshare.security.filter.AppAuthenticationFilter;
 import org.bs.bookshare.security.filter.AppAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -27,6 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final CustomAuthenticationProvider authenticationProvider;
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment environment;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
@@ -37,7 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilter(new AppAuthenticationFilter(authenticationManagerBean()));
+        http.addFilter(new AppAuthenticationFilter(authenticationManagerBean(), environment));
         http.addFilterBefore(new AppAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
--- Bookshare/src/main/java/org/bs/bookshare/security/filter/AppAuthenticationFilter.java
+++ Bookshare/src/main/java/org/bs/bookshare/security/filter/AppAuthenticationFilter.java
@@ -3,8 +3,10 @@ package org.bs.bookshare.security.filter;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.bs.bookshare.auth.CustomAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -21,16 +23,15 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 public class AppAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
 
-
-    public AppAuthenticationFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-    }
+    private final Environment environment;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -47,13 +48,13 @@ public class AppAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());  //TODO zakodować to
         String accessToken = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000)) //TODO wczytywanie z pliku
+                .withExpiresAt(new Date(System.currentTimeMillis() + (long) Integer.parseInt(Objects.requireNonNull(environment.getProperty("access_token.valid_time_in_minutes"))) * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
-        
-        Map<String,String> responseToken = new HashMap<>();
-        responseToken.put("access_token",accessToken);
+
+        Map<String, String> responseToken = new HashMap<>();
+        responseToken.put("access_token", accessToken);
         response.setContentType("application/json");
         new ObjectMapper().writeValue(response.getOutputStream(), responseToken);
 
@@ -61,12 +62,12 @@ public class AppAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-        Map<String,String> responseMap = new HashMap<>();
+        Map<String, String> responseMap = new HashMap<>();
         response.setStatus(401);
-        responseMap.put("status",String.valueOf(response.getStatus()));
-        responseMap.put("error",failed.getMessage());
-        responseMap.put("message",failed.getMessage());
+        responseMap.put("status", String.valueOf(response.getStatus()));
+        responseMap.put("error", failed.getMessage());
+        responseMap.put("message", failed.getMessage());
         response.setContentType("application/json");
-        new ObjectMapper().writeValue(response.getOutputStream(),responseMap);
+        new ObjectMapper().writeValue(response.getOutputStream(), responseMap);
     }  //TODO
 }
