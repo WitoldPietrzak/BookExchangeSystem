b5c4586c7e5761edcd1876a1f31b851ff04426cb
WitoldPietrzak <witekpietrzak35@gmail.com>
2022-01-19 02:21:05 +0100

    missing file

--- bookshare_front/src/Views/MainView.js
+++ bookshare_front/src/Views/MainView.js
@@ -5,8 +5,11 @@ import {MDBContainer, MDBFooter} from "mdbreact";
 import BreadCrumbs from "../Components/BreadCrumbs/BreadCrumbs";
 import './MainView.css';
 import {default as SiteNavbar} from "../Components/Navbar/Navbar"
-import Router from "../Routes/Router";
+import Router, {isLogged} from "../Routes/Router";
 import i18n from "i18next";
+import {makeRefreshTokenRequest} from "../Requests/mok/RefreshTokenRequest";
+import {Button, Modal} from "react-bootstrap";
+import {checkReadiness} from "../Requests/mok/ReadinessRequest";
 
 export const jwtCookieExpirationTime = process.env.REACT_APP_FRONT_JWT_EXPIRATION_MS / (24 * 60 * 60 * 100)
 export const activeRole = Cookies.get(process.env.REACT_APP_FRONT_ACTIVE_ROLE_COOKIE_NAME) !== undefined ? Cookies.get(process.env.REACT_APP_FRONT_ACTIVE_ROLE_COOKIE_NAME) : 'ROLE_GUEST';
@@ -15,21 +18,40 @@ class MainViewNoTr extends React.Component {
 
     constructor(props) {
         super(props);
+        const {t} = this.props;
+        this.readinessId = undefined;
         this.state = {
-            location: '',
+            refreshError: false,
+            message: '',
+            checkForReadiness: true
         };
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(loc => {
-                this.setState({
-                    location: `lat: ${loc.coords.latitude} len:${loc.coords.longitude}`
-                })
-            })
-        }
+        document.title = t("Site.name");
         Cookies.get(process.env.REACT_APP_LANGUAGE_COOKIE_NAME) === undefined ? Cookies.set(process.env.REACT_APP_LANGUAGE_COOKIE_NAME, i18n.language, {
             expires: jwtCookieExpirationTime,
             secure: true,
             sameSite: 'none'
         }) : i18n.changeLanguage(Cookies.get(process.env.REACT_APP_LANGUAGE_COOKIE_NAME));
+    }
+
+    componentDidMount() {
+        if (isLogged()) {
+            setInterval(() => {
+                makeRefreshTokenRequest(this)
+            }, parseInt(process.env.REACT_APP_FRONT_REFRESH_TIME_MS));
+        }
+        if (this.state.checkForReadiness === true) {
+            this.readinessId = setInterval(() => {
+                    checkReadiness(this);
+                }, parseInt(process.env.REACT_APP_FRONT_READINESS_CHECK_TIME_MS)
+            );
+        }
+
+    }
+
+    hideModal() {
+        this.setState({
+            refreshError: false
+        })
     }
 
 
@@ -40,20 +62,27 @@ class MainViewNoTr extends React.Component {
                 <SiteNavbar/>
                 <BreadCrumbs/>
                 <div className='View'>
-                <Router/>
+                    <Router/>
                 </div>
-                {/*<div>*/}
-                {/*    {this.state.location} <br />*/}
-                {/*</div>*/}
-
                 <MDBFooter color="blue" className="font-small pt-4 mt-4 bottom" id="footer">
                     <div className="footer-copyright text-right py-3">
                         <MDBContainer>
                             BookCross, &copy; {new Date().getFullYear()} Copyright by Witold Pietrzak
+
                         </MDBContainer>
                     </div>
                 </MDBFooter>
+                <Modal show={this.state.refreshError} onHide={this.hideModal.bind(this)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{t('Error.Title')}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>{t(this.state.message)}</Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.hideModal.bind(this)}>{t('Button.Close')}</Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
+
         );
     }
 }
