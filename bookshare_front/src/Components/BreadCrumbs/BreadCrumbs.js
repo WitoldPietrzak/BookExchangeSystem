eea89df8a849b642db4a47abc3d62c38cff0877e
WitoldPietrzak <witekpietrzak35@gmail.com>
2022-01-20 00:12:26 +0100

    Fixed Breadcrumbs

--- bookshare_front/src/Components/BreadCrumbs/BreadCrumbs.js
+++ bookshare_front/src/Components/BreadCrumbs/BreadCrumbs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useLocation, useNavigate, Link} from "react-router-dom";
+import {useLocation, Link} from "react-router-dom";
 import {Breadcrumbs} from "@material-ui/core";
 import './BreadCrumbs.css';
 
@@ -14,7 +14,6 @@ function BreadCrumbs() {
 function HeaderView() {
     const str = useLocation().pathname.substring(1).split('/');
     let rows = [];
-    const history = useNavigate();
     for (let i = 0; i < str.length; i++) {
         let url_str = [];
         for (let j = 0; j <= i; j++) {
@@ -22,11 +21,10 @@ function HeaderView() {
             url_str.push(str[j]);
         }
         let joined_str = url_str.join("");
-        const handleOnClick = () => history.push(joined_str);
         rows.push(
             <span key = {str[i]}>
                 {" "} / {" "}
-                <Link to={joined_str} href="#" onClick={handleOnClick}>
+                <Link to={joined_str} href="#" >
                     {/*{str[i-1]=='reset-password' || str[i-1]=='activate' ? 'token':str[i]}*/}
                     {str[i].length >60 ? str[i].substring(0, 30)+'...':str[i]}
                 </Link>
