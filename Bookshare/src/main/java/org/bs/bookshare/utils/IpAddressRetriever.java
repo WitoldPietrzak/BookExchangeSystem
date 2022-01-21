91aabe94f501e398f1febde8cdb7fd2b0f360cf0
WitoldPietrzak <witekpietrzak35@gmail.com>
2022-01-21 02:32:18 +0100

    missing file

--- Bookshare/src/main/java/org/bs/bookshare/utils/IpAddressRetriever.java
+++ Bookshare/src/main/java/org/bs/bookshare/utils/IpAddressRetriever.java
@@ -1,6 +1,13 @@
 package org.bs.bookshare.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
 
 public class IpAddressRetriever {
     private static final String[] HEADERS_TO_TRY = {
@@ -17,6 +24,18 @@ public class IpAddressRetriever {
             "REMOTE_ADDR"};
 
     public static String getClientIpAddressFromHttpServletRequest(HttpServletRequest request) {
+        for (String header : HEADERS_TO_TRY) {
+            String ip = request.getHeader(header);
+            if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+                return ip;
+            }
+        }
+        return request.getRemoteAddr();
+    }
+
+    @Bean
+    public static String getClientIpAddressFromHttpServletRequest() {
+        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
         for (String header : HEADERS_TO_TRY) {
             String ip = request.getHeader(header);
             if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
