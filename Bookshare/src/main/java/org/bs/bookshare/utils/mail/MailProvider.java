851de4c021a2fadde744213451e50b088404da21
WitoldPietrzak <witekpietrzak35@gmail.com>
2022-01-22 03:23:38 +0100

    Fixed mail messages links, added some missing translations

--- Bookshare/src/main/java/org/bs/bookshare/utils/mail/MailProvider.java
+++ Bookshare/src/main/java/org/bs/bookshare/utils/mail/MailProvider.java
@@ -32,39 +32,45 @@ public class MailProvider {
     @Autowired
     Environment environment;
 
-    public void sendActivationMail(String to, String token, String lang){
+    public void sendActivationMail(String to, String token, String lang) {
         Locale locale = new Locale(lang);
+        StringBuilder stringBuilder = new StringBuilder();
+        String activationLink = String.format(stringBuilder.append(environment.getProperty("frontend_address_base")).append(environment.getProperty("frontend_activation_path")).toString(), token);
         ResourceBundle langBundle = ResourceBundle.getBundle("LanguageRes", locale);
         String subject = langBundle.getString(ACTIVATION_MAIL_SUBJECT);
-        String from =environment.getProperty("spring.mail.username");
-        String message = String.format(langBundle.getString(ACTIVATION_MAIL_TEXT), token);
-        emailService.sendMail(to,subject,from,message);
+        String from = environment.getProperty("spring.mail.username");
+        String message = String.format(langBundle.getString(ACTIVATION_MAIL_TEXT), activationLink);
+        emailService.sendMail(to, subject, from, message);
     }
 
     public void sendPasswordResetMail(String to, String token, String lang) {
         Locale locale = new Locale(lang);
+        StringBuilder stringBuilder = new StringBuilder();
+        String activationLink = String.format(stringBuilder.append(environment.getProperty("frontend_address_base")).append(environment.getProperty("frontend_reset_path")).toString(), token);
         ResourceBundle langBundle = ResourceBundle.getBundle("LanguageRes", locale);
         String subject = langBundle.getString(PASSWORD_RESET_MAIL_SUBJECT);
-        String from =environment.getProperty("spring.mail.username");
-        String message = String.format(langBundle.getString(PASSWORD_RESET_MAIL_TEXT), token);
-        emailService.sendMail(to,subject,from,message);
+        String from = environment.getProperty("spring.mail.username");
+        String message = String.format(langBundle.getString(PASSWORD_RESET_MAIL_TEXT), activationLink);
+        emailService.sendMail(to, subject, from, message);
     }
 
     public void sendAccountEnableMail(String to, String token, String lang) {
         Locale locale = new Locale(lang);
+        StringBuilder stringBuilder = new StringBuilder();
+        String activationLink = String.format(stringBuilder.append(environment.getProperty("frontend_address_base")).append(environment.getProperty("frontend_enable_path")).toString(), token);
         ResourceBundle langBundle = ResourceBundle.getBundle("LanguageRes", locale);
         String subject = langBundle.getString(ACCOUNT_ENABLE_MAIL_SUBJECT);
-        String from =environment.getProperty("spring.mail.username");
-        String message = String.format(langBundle.getString(ACCOUNT_ENABLE_MAIL_TEXT), token);
-        emailService.sendMail(to,subject,from,message);
+        String from = environment.getProperty("spring.mail.username");
+        String message = String.format(langBundle.getString(ACCOUNT_ENABLE_MAIL_TEXT), activationLink);
+        emailService.sendMail(to, subject, from, message);
     }
 
     public void sendAdminLoginAttemptMail(String to, String ip, String lang) {
         Locale locale = new Locale(lang);
         ResourceBundle langBundle = ResourceBundle.getBundle("LanguageRes", locale);
         String subject = langBundle.getString(ADMIN_ACCOUNT_LOGIN_MAIL_SUBJECT);
-        String from =environment.getProperty("spring.mail.username");
+        String from = environment.getProperty("spring.mail.username");
         String message = String.format(langBundle.getString(ADMIN_ACCOUNT_LOGIN_MAIL_TEXT), ip);
-        emailService.sendMail(to,subject,from,message);
+        emailService.sendMail(to, subject, from, message);
     }
 }
