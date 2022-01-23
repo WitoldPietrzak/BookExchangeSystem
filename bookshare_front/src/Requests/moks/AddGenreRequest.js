6b54ae28261f1352de7b3a5e81c549212b643d2c
WitoldPietrzak <witekpietrzak35@gmail.com>
2022-01-23 11:43:19 +0100

    Created component for adding new genres

---
+++ bookshare_front/src/Requests/moks/AddGenreRequest.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import Cookies from 'js-cookie';
+
+export function makeAddGenreRequest(token, nameCode, names, that) {
+    const namesToSend = {};
+    names.forEach((name) => {
+        namesToSend[`${name.key}`] = name.value
+    })
+
+    axios.post(`${process.env.REACT_APP_BACKEND_URL}/genre/add`, {
+        nameCode: nameCode,
+        names: namesToSend,
+    }, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    }).then((response) => {
+        window.location.hash = "#/home";
+        window.location.reload();
+    }).catch((response) => {
+        if (response.response) {
+            if (response.response) {
+                that.setState({
+                    button: 'Form.registerButton',
+                    errorCode: response.response.status.toString(10),
+                    response: response.response.data.message,
+                    requestFailed: true
+                })
+            }
+        }
+    })
+}
--- bookshare_front/src/Routes/Router.js
+++ bookshare_front/src/Routes/Router.js
@@ -13,6 +13,8 @@ import AdminAccountEdit from "../Components/AccountEdit/AdminAccountEdit";
 import Logs from "../Components/Logs/Logs";
 import BookshelfList from "../Components/BookshelfList/BookshelfList";
 import Bookshelf from "../Components/Bookshelf/Bookshelf";
+import BookshelfAdd from "../Components/BookshelfAdd/BookshelfAdd";
+import GenreAdd from "../Components/GenreAdd/GenreAdd";
 
 let token = Cookies !== undefined ? Cookies.get(process.env.REACT_APP_FRONT_JWT_TOKEN_COOKIE_NAME) : undefined;
 const role = token ? Cookies.get(`${process.env.REACT_APP_ACTIVE_ROLE_COOKIE_NAME}_${Cookies.get(process.env.REACT_APP_FRONT_LOGIN_COOKIE_NAME)}`) : undefined;
@@ -51,6 +53,8 @@ export default function Router() {
             <Route exact path='/accounts' element={isAdmin() ? <AccountList/> : <Navigate replace to='/home'/>}/>
             <Route exact path='/shelves' element={isModerator() || isUser() ? <BookshelfList/> : <Navigate replace to='/home'/>}/>
             <Route exact path='/shelves/:id' element={isModerator() || isUser() ? <Bookshelf/> : <Navigate replace to='/home'/>}/>
+            <Route exact path='/shelves/add' element={isModerator() ? <BookshelfAdd/> : <Navigate replace to='/home'/>}/>
+            <Route exact path='/genres/add' element={isModerator() ? <GenreAdd/> : <Navigate replace to='/home'/>}/>
             <Route path='*' element={<Navigate replace to={'/home'}/>}/>
         </Routes>
     )
