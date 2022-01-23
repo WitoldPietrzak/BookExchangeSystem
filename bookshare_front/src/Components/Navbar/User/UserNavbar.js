b183c4127b88c879defa16c1494f65391266beac
WitoldPietrzak <witekpietrzak35@gmail.com>
2022-01-23 14:21:43 +0100

    reworked user navbar

--- bookshare_front/src/Components/Navbar/User/UserNavbar.js
+++ bookshare_front/src/Components/Navbar/User/UserNavbar.js
@@ -2,13 +2,23 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import {useTranslation} from "react-i18next";
 import {Link} from "react-router-dom";
+import {NavDropdown} from "react-bootstrap";
 
 function User() {
     const {t} = useTranslation();
     return (
         <Nav activeKey={window.location.hash}>
-            <Nav.Link as={Link} to="/shelves">{t("Navbar.bookshelves")}</Nav.Link>
-            <Nav.Link as={Link} to="/books">{t("Navbar.books")}</Nav.Link>
+            <NavDropdown title={t("Navbar.bookshelves")} id={'xd'}>
+                <Nav.Link className="navStyle" style={{color: "rgb(127, 127, 127)"}}
+                          href="/?#/shelves">{t("Navbar.bookshelves.list")}</Nav.Link>
+            </NavDropdown>
+            <NavDropdown title={t("Navbar.books")} id="navbarScrollingDropdown">
+                <Nav.Link className="navStyle" style={{color: "rgb(127, 127, 127)"}}
+                          href="/#/books/add">{t("Navbar.books.add")}</Nav.Link>
+                <NavDropdown.Divider/>
+                <Nav.Link className="navStyle" style={{color: "rgb(127, 127, 127)"}}
+                          href="/?#/books">{t("Navbar.books.list")}</Nav.Link>
+            </NavDropdown>
         </Nav>
     );
 }
