f4e9f3f5a2b9660d09577aa17963f3acdb14f2b1
WitoldPietrzak <witekpietrzak35@gmail.com>
2022-01-23 14:20:00 +0100

    renamed DTO

--- Bookshare/src/main/java/org/bs/bookshare/moks/dto/request/SimpleBookCopyRequestDTO.java
+++ Bookshare/src/main/java/org/bs/bookshare/moks/dto/request/SimpleBookCopyRequestDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class TakeBookCopyFromShelfRequestDTO {
+public class SimpleBookCopyRequestDTO {
     private Long bookCopyId;
     private Long version;
 }
